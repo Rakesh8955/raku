4651d80acedb38bc83b346cb7cfd9f7bf7c3a00d
Rakesh8955 <145524293+Rakesh8955@users.noreply.github.com>
2024-09-25 08:17:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,234 @@
+
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"/>
+<meta name="google" content="notranslate" />
+<title><?php echo preg_replace('/[+--" \']/', ' ', @$_GET["n"]); if (!@$_GET["n"]) echo "[Seu nome]";?></title>
+    <meta property="og:type" content="😀😀😍😍" />
+    <meta property="og:title" content="<?php echo preg_replace('/[+--" \']/', ' ', @$_GET["n"]); if (!@$_GET["n"]) echo "[Seu nome]";?>" />
+    <meta property="og:url" content="https://br.shikshasuchna.com/" />
+    <meta property="og:description" content="create Christmas greeting card and share with friends." >
+    <meta property="og:site_name" content="😀😀😍😍" />
+    <meta property="og:image" content="img/CH7.png" />
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="snowfall.css">
+     <link rel = "icon" href =  "img/CH7.png" type = "image/x-icon">
+     <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-221572847-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-221572847-1');
+</script>
+<style>
+.image-container {
+ width: 50%;
+height: 100%;
+top: 0px;
+position: absolute;
+transition: transform 2s ease;
+}
+
+.image-container:nth-child(1) {
+left: 0;
+z-index: 2;
+transform-origin: left center;
+}
+
+.image-container:nth-child(2) {
+right: 0;
+z-index: 2;
+transform-origin: right center;
+}
+
+.image-container img {
+width: 100%;
+height: 100%;
+object-fit: cover;
+}
+
+.text-container {
+position: absolute;
+z-index: 4;
+top: 30%;
+left: 50%;
+transform: translate(-50%, -50%);
+text-align: center;
+}
+
+.touch-here{
+font-size: 30px;
+animation: pulse 1s infinite;
+text-shadow: 1px 1px 3px black, 1px 1px 3px black, -1px -1px 3px black, -1px -1px 3px black;
+color: green;
+font-weight: bold;
+}
+
+.hidden {
+display: none;
+}
+.click-here {
+	animation: jello 1s infinite;
+	
+	}
+@keyframes jello {  from, 11.1%, to {transform: none;  }
+  22.2% {transform: skewX(-12.5deg) skewY(-12.5deg);  }
+  33.3% {transform: skewX(6.25deg) skewY(6.25deg);  }
+  44.4% {transform: skewX(-3.125deg) skewY(-3.125deg);  }
+  55.5% {transform: skewX(1.5625deg) skewY(1.5625deg);  }
+  66.6% {transform: skewX(-0.78125deg) skewY(-0.78125deg);  }
+  77.7% {transform: skewX(0.390625deg) skewY(0.390625deg);  }
+  88.8% {transform: skewX(-0.1953125deg) skewY(-0.1953125deg);  }}
+ 
+</style>
+</head>
+<body style="background-color: #0C090A; background-image: url(img/bgstart.gif)">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+$(document).ready(function() {
+const touchHere = $('.touch-here');
+const clickHere = $('.click-here');
+const imageContainers = $('.image-container');
+const audio = $('#background-music');
+
+clickHere.on('click', function() {
+touchHere.addClass('hidden');
+clickHere.addClass('hidden');
+audio[0].play();
+
+imageContainers.css('transform', 'rotateY(90deg)');
+
+setTimeout(function() {
+imageContainers.css('display', 'none');
+}, 2000);
+});
+});
+</script>
+<div class="container">
+<div class="image-container">
+<img src="img/d1.jpg" loading="eager>
+1.jpg" loading="eager"/>
+</div>
+<div class="image-container">
+<img src="img/d2.jpg" loading="eager>
+2.jpg" loading="eager"/>
+</div>
+<div class="text-container">
+<p class="touch-here"><br><br>toque nisso<br>👇👇👇</b></p><br><br><br><br><br>
+<img class="click-here" src="img/bear.png" alt="Image 1" style="width:100%; width: 100px; animation: bounceIn 4s infinite;">
+
+</div>
+</div>
+
+<audio id="background-music">
+<source src="MerryChristmas.mp3" type="audio/mpeg">
+</audio>
+<div class="m1" behavior="" direction="" scrolldelay="0"> <br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.2s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.1s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1s"/><br><br>
+<img src="img/4.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.9s"/><br><br>
+<img src="img/5.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.8s"/><br><br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.7s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.6s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.5s"/><br><br>
+<img src="img/4.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.4s"/><br><br>
+<img src="img/5.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.3s"/><br><br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.2s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.1s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:0.0s"/><br><br>
+</div>
+<div class="m2" behavior="" direction="" scrolldelay="0"> <br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.2s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.3s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.4s"/><br><br>
+<img src="img/4.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.5s"/><br><br>
+<img src="img/5.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.6s"/><br><br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.7s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.8s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.9s"/><br><br>
+<img src="img/4.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:2.0s"/><br><br>
+<img src="img/5.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:2.1s"/><br><br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:2.2s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:2.3s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:2.4s"/><br><br>
+</div>
+<main>
+<div class="container"><br>
+<center>
+   
+</center><br>
+<center>
+<?php
+$yourName = urldecode($_GET['n']);
+
+if (empty($yourName)) {
+    $yourName = "Seu\nnome";
+}
+
+$yourName = str_replace('-', "\n\n", $yourName);
+
+$characters = preg_split('//u', $yourName, -1, PREG_SPLIT_NO_EMPTY);
+
+echo '<div class="test" style="font-family: SF Espresso Shack;"><div class="name">';
+
+foreach ($characters as $char) {
+    if ($char === "\n") {
+        echo '<br>'; // Output a line break in HTML
+    } else {
+        echo '<span>' . $char . '</span>';
+    }
+}
+
+echo '</div></div>';
+?>
+</center>
+<center><img src="img/wish1.png" width="50%" height="auto"></center><br>
+
+<p id="demo"></p><br>
+
+<img src="img/felizs.png" width="75%" height="35%" style="animation: jello 1s infinite"> <br><br>
+<img src="img/tree.png" width="60%" height="auto" style="animation: pulse 2s infinite;"><br><br>
+<div class="GodMessage" style="text-shadow: 1px 1px 3px yellow, 1px 1px 3px orange, -1px -1px 3px silver, -1px -1px 3px black;"> 
+<p style="text-shadow: 4px 4px 8px;color:#C619F9">Que o Natal traga felicidade</p>
+<p style="text-shadow: 4px 4px 8px;color:#ff9933">e um ano-novo mais bonito</p>
+<p style="text-shadow: 4px 4px 8px;color:yellow">que a paz tenha prioridade</p>
+<p style="text-shadow: 4px 4px 8px;color:red">e o amor seja infinito</p>
+</div><br>
+<center>
+    
+</center><br>
+<div id='snowfall'>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+</div>
+<br>
+<center>
+   <div class="neon-button"><?php echo preg_replace('/[+--" \']/', ' ', @$_GET["n"]); if (!@$_GET["n"]) echo "[Seu nome]";?></div></center>
+<br><br>
+
+</main>
+<div class="footer">    
+    <form method="post" action="ready.php">
+      <input class="text" name="n" type="text" placeholder="👉 insira seu nome.." required>
+      <input type="submit" class="go" value="👉 ">
+    </form>
+  </div>
+<style>
+input::placeholder {
+  color: white;
+  text-align: center;
+}
+</style>
+
+<script> 
+var countDownDate=new Date("dec 25, 2023 00:00:00").getTime(),x=setInterval(function(){var e=(new Date).getTime(),o=countDownDate-e,t=Math.floor(o/864e5),n=Math.floor(o%864e5/36e5),a=Math.floor(o%36e5/6e4),l=Math.floor(o%6e4/1e3);document.getElementById("demo").innerHTML=""+t+"<font color='#048505'> dias,</font> "+n+"<font color='#a71780'> horas,</font> "+a+"<font color='#6666ff'> min,<br></font> "+l+"<font color='#c9850d'> segundos,</font> <font color='#ff1aff'> antes</font>",o<0&&(clearInterval(x),document.getElementById("demo").innerHTML="")},1e3);
+</script>
+</body>
+</html>
---
+++ ready.php
@@ -0,0 +1,232 @@
+<?php
+@$n = @$_POST["n"];
+@$name = str_replace(
+array(" ","@","gmail.com","yahoo.com"),
+array(" ","","",""), @$n);
+@$yname = str_replace(
+array(" ","@","gmail.com","yahoo.com"),
+array("-","","",""), @$n);
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"/>
+<meta name="google" content="notranslate" />
+<title>christmas card</title>
+    <meta property="og:type" content="christmas card" />
+    <meta property="og:title" content="christmas card" />
+    <meta property="og:url" content="https://br.shikshasuchna.com/" />
+    <meta property="og:description" content="christmas card " >
+    <meta property="og:site_name" content="create Christmas greeting card and share with friends." />
+    <meta property="og:image" content="ogmsg.jpg" />
+    <link rel="stylesheet" href="style.css">
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-221572847-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-221572847-1');
+</script>
+<style>
+ .image-container {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      grid-column-gap: 10px;
+      justify-content: space-between;
+    }
+    
+    @keyframes dance{
+0%{
+-webkit-transform:rotate(10deg);
+-ms-transform:rotate(10deg);
+transform:rotate(10deg)
+}
+16.66%,49.98%{
+-webkit-transform:rotate(-10deg);
+-ms-transform:rotate(-10deg);
+transform:rotate(-10deg)
+}
+32.32%{
+-webkit-transform:rotate(-5deg);
+-ms-transform:rotate(-5deg);
+transform:rotate(-5deg)
+}
+100%,66.64%{
+-webkit-transform:rotate(10deg);
+-ms-transform:rotate(10deg);
+transform:rotate(10deg)
+}
+83.8%{
+-webkit-transform:rotate(5deg);
+-ms-transform:rotate(5deg);
+transform:rotate(5deg)
+}}
+
+-webkit-@keyframes dance{
+-ms-transform:rotate(10deg);
+transform:rotate(10deg)
+}
+16.66%,49.98%{
+-webkit-transform:rotate(-10deg);
+-ms-transform:rotate(-10deg);
+transform:rotate(-10deg)
+}
+32.32%{
+-webkit-transform:rotate(-5deg);
+-ms-transform:rotate(-5deg);
+transform:rotate(-5deg)
+}
+100%,66.64%{
+-webkit-transform:rotate(10deg);
+-ms-transform:rotate(10deg);
+transform:rotate(10deg)
+}
+83.8%{
+-webkit-transform:rotate(5deg);
+-ms-transform:rotate(5deg);
+transform:rotate(5deg)
+}
+
+</style>
+</head>
+<body style="background-color: #0C090A; background-image: url(img/bgstart.gif)">
+<div class="m1" behavior="" direction="" scrolldelay="0"> <br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.2s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.1s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1s"/><br><br>
+<img src="img/4.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.9s"/><br><br>
+<img src="img/5.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.8s"/><br><br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.7s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.6s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.5s"/><br><br>
+<img src="img/4.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.4s"/><br><br>
+<img src="img/5.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.3s"/><br><br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.2s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:.1s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:0.0s"/><br><br>
+</div>
+<div class="m2" behavior="" direction="" scrolldelay="0"> <br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.2s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.3s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.4s"/><br><br>
+<img src="img/4.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.5s"/><br><br>
+<img src="img/5.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.6s"/><br><br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.7s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.8s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:1.9s"/><br><br>
+<img src="img/4.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:2.0s"/><br><br>
+<img src="img/5.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:2.1s"/><br><br>
+<img src="img/1.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:2.2s"/><br><br>
+<img src="img/2.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:2.3s"/><br><br>
+<img src="img/3.png" height="30px" width="26px" style="animation: blink 2s infinite; animation-delay:2.4s"/><br><br>
+</div>
+<main>
+<div class="container">
+    <br>
+<center>
+   
+</center><br>
+<center>
+<div class="test" style="font-family: SF Espresso Shack;">
+        <div class="name">
+            <?php
+            if (isset($_POST['n'])) {
+                $userName = $_POST['n'];
+                // Replace spaces with line breaks for the name and surname
+                $userName = str_replace(' ', "\n", $userName);
+                // Split the user's name into individual characters
+                $characters = preg_split('//u', $userName, -1, PREG_SPLIT_NO_EMPTY);
+                foreach ($characters as $char) {
+                    if ($char === "\n") {
+                        echo '<br>'; // Output a line break in HTML
+                    } else {
+                        echo '<span>' . $char . '</span>';
+                    }
+                }
+            } else {
+                echo 'Name not provided.';
+            }
+            ?>
+        </div>
+</div>
+</center>
+<center><img src="img/wish1.png" width="50%" height="auto"></center><br>
+<div style="font-size: 18px; font-weight: 700; color: white;">
+<p id="demo"></p></div>
+<img src="img/felizs.png" width="75%" height="35%" style="animation: jello 1s infinite"> <br><br>
+<img src="img/tee.gif" width="70%" height="30%" style=""><br><br>
+<center>
+    
+</center><br>
+<div class="GodMessage"> 
+<p style="text-shadow: 4px 4px 8px;color:#C619F9">Que a alegria do Natal invada</p>
+<p style="text-shadow: 4px 4px 8px;color:#ff9933">sua casa e faça morada por todos</p>
+ <span style="text-shadow: 4px 4px 8px;color:white">os dias do novo ano</p>
+ <span style="text-shadow: 4px 4px 8px;color:green"> Feliz Natal e Feliz Ano-Novo!</p>
+<br>
+</center><br>
+</main>
+
+<a class="shareBtn" id="div1" href="#platforms">Compartilhe com amigos</a>
+<a class="shareShow" id="div2" href="#close" style="display: none;">Fechar</a>
+
+<div class="platforms" id="platforms">
+<center>
+<a href="whatsapp://send?text= 🤶 *<?php echo str_replace(
+    array(" ","@","gmail.com","yahoo.com"),
+    array("-","","",""), @$n)?>* 🤶 %0A Enviei esta mensagem para você primeiro. por favor me dê feedback também  %0A toque nisso 👇🏻👇🏻 %0A https://br.shikshasuchna.com/?n=<?php echo str_replace(
+    array(" ","@","gmail.com","yahoo.com"),
+    array("-","","",""), @$n)?>" target="_blank" data-os="Whatsapp"><img src="img/wtsp.png" style="animation: dance 2s infinite;margin-top:5px;height: 50px;width:50px;"></a>
+<a href="fb-messenger://share/?link=https://br.shikshasuchna.com/?n=<?php echo str_replace(
+    array(" ","@","gmail.com","yahoo.com"),
+    array("-","","",""), @$n)?>" data-os="Messenger"><img src="img/ms.png" style="animation: dance 2s infinite;margin-top:5px;height: 50px;width:50px;"></a></center>
+</div>
+
+<script>
+// Click to show and hide share buttons
+$(document).ready(function(){
+$(".shareBtn").click(function(){
+$(".shareBtn").hide();
+$(".shareShow").show();
+});
+$(".shareShow").click(function(){
+$(".shareShow").hide();
+$(".shareBtn").show();
+});
+});
+</script>
+
+<script> 
+// Set the date we're counting down to
+var countDownDate = new Date("dec 25, 2023 00:00:00").getTime();
+
+// Update the count down every 01 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "<font color='#FA069A'>  jours,</font> " + hours + "<font color='#11680B'> h,</font> "
+  + minutes + "<font color='#9B274C'>  min,<br></font> " + seconds + "<font color='#F47810'> sec avant</font> ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "";
+  }
+}, 1000);
+</script>
+</body>
+</html>
